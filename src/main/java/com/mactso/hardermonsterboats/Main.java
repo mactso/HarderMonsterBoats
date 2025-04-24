daf18ec61c15ce659ee9cff1b465745c5e533c35
Mac <Mac@Coke.lan>
2025-04-23 20:35:46 -0500

    1.21.4 for Forge

--- src/main/java/com/mactso/hardermonsterboats/Main.java
+++ src/main/java/com/mactso/hardermonsterboats/Main.java
@@ -2,10 +2,10 @@ package com.mactso.hardermonsterboats;
 
 import com.mactso.hardermonsterboats.config.MyConfig;
 import com.mactso.hardermonsterboats.events.EventHandler;
+import com.mactso.hardermonsterboats.util.Utility;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -16,12 +16,11 @@ public class Main {
 
 	    public static final String MODID = "hardermonsterboats"; 
 	    
-	    public Main()
+	    public Main(FMLJavaModLoadingContext context)
 	    {
-	  		FMLJavaModLoadingContext.get().getModEventBus().register(this);
-	  		
-	    	System.out.println(MODID + ": Registering Mod.");
- 	        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON,MyConfig.COMMON_SPEC );
+			context.getModEventBus().register(this);
+			context.registerConfig(ModConfig.Type.COMMON, MyConfig.COMMON_SPEC);
+			Utility.debugMsg (0, MODID + ": Registering Mod");
 	    }
 	    
 		@SubscribeEvent 
