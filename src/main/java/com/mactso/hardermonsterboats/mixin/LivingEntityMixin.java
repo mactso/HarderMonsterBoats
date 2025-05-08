74395cdd310f9b01fb35f2e1ea4aace06f2f184a
Mac <Mac@Coke.lan>
2025-05-07 20:33:10 -0500

    Version 7 for 1.21.4 for Fabric

--- src/main/java/com/mactso/hardermonsterboats/mixin/LivingEntityMixin.java
+++ src/main/java/com/mactso/hardermonsterboats/mixin/LivingEntityMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mactso.hardermonsterboats.events.EventHandler;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 
@@ -14,7 +15,7 @@ import net.minecraft.world.entity.LivingEntity;
 abstract class LivingEntityMixin {
 
 	@Inject(method = "actuallyHurt", at = @At("HEAD"))
-	private void onEntityActuallyHurt(DamageSource source, float amount, CallbackInfo info) {
+	private void onEntityActuallyHurt(ServerLevel serverLevel, DamageSource source, float amount, CallbackInfo info) {
 		EventHandler.onEntityActuallyHurt((LivingEntity) (Object) this);
 	}
 }
--- src/main/java/com/mactso/hardermonsterboats/util/Utility.java
+++ src/main/java/com/mactso/hardermonsterboats/util/Utility.java
@@ -8,8 +8,8 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 
 public class Utility {
 	public final static int FOUR_SECONDS = 80;
@@ -45,17 +45,16 @@ public class Utility {
 
 	}
 
-	public static void sendBoldChat(Player p, String chatMessage, ChatFormatting textColor) {
+	public static void sendBoldChat(ServerPlayer p, String chatMessage, ChatFormatting textColor) {
 
 		MutableComponent component = Component.literal(chatMessage);
 		component.setStyle(component.getStyle().withBold(true));
 		component.setStyle(component.getStyle().withColor(textColor));
 		p.sendSystemMessage(component);
 
-
 	}
 
-	public static void sendChat(Player p, String chatMessage, ChatFormatting textColor) {
+	public static void sendChat(ServerPlayer p, String chatMessage, ChatFormatting textColor) {
 
 		MutableComponent component = Component.literal(chatMessage);
 		component.setStyle(component.getStyle().withColor(textColor));
